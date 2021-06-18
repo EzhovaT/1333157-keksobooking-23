960b412f704dfba3c381714788a347b8a307c6c4
Tatyana <tatyezhova@yandex.ru>
2021-06-18 16:01:38 +0300

    Генерация разметки похожих объявлений

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 import {ANNOUNCEMENT_COUNT} from './data.js';
 import {createAnnouncement} from './create-announcement.js';
+import {generationMarkupCards} from './generating-markup-cards.js';
+
+const map = document.querySelector('#map-canvas');
 
 const similarAnnouncements =  new Array(ANNOUNCEMENT_COUNT)
   .fill('')
   .map((currentValue, index) => createAnnouncement(index));
 
-similarAnnouncements;
+const markupCards = generationMarkupCards(similarAnnouncements);
+
+map.appendChild(markupCards.children[0]);
