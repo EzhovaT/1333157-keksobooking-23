5efc9fafdc78c7a639ba12c548ef311906827fb5
Tatyana <tatyezhova@yandex.ru>
2021-06-24 12:29:27 +0300

    Добавление валидации

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {ANNOUNCEMENT_COUNT} from './data.js';
 import {createAnnouncement} from './create-announcement.js';
 import {generateMarkupCards} from './generate-markup-cards.js';
+import {checkingNumberOfGuests, addMinimumCostHousing} from './form.js';
 
 const map = document.querySelector('#map-canvas');
 
@@ -11,3 +12,7 @@ const similarAnnouncements =  new Array(ANNOUNCEMENT_COUNT)
 const markupCards = generateMarkupCards(similarAnnouncements);
 
 map.appendChild(markupCards.children[1]);
+
+const buttonFormSubmit = document.querySelector('.ad-form__submit');
+buttonFormSubmit.addEventListener('click', checkingNumberOfGuests);
+buttonFormSubmit.addEventListener('click', addMinimumCostHousing);
